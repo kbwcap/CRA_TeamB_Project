1e21b15a4971fb1c858f7d3c73a229ff51ac51e9
raina <raina.bak@gmail.com>
2025-04-02 13:46:33 +0900

    CommandFixture 클래스 추가.

--- TeamB_SSD/SSD_Test.cpp
+++ TeamB_SSD/SSD_Test.cpp
@@ -2,59 +2,47 @@
 #include "gmock/gmock.h"
 #include "iostream"
 
-TEST(SSDTEST, basic_SSD_test_Write_1) {
-  char c = 'W';
-  int num = 3;
-  uint32_t hexValue = 0xAAAABBBB;
+using namespace testing;
+
+class CommandFixture : public Test {
+ protected:
   VirtualSSD ssd;
-  bool ret = ssd.executeCommand(c, num, hexValue);
-  EXPECT_TRUE(ret);
+
+  void executeAndExpectTRUE(char cmd, int lba, uint32_t value) {
+    bool ret = ssd.executeCommand(cmd, lba, value);
+    EXPECT_TRUE(ret);
+  }
+
+  void expectCommandFALSE(char cmd, int lba, uint32_t value) {
+    bool ret = ssd.executeCommand(cmd, lba, value);
+    EXPECT_FALSE(ret);
+  }
+
+ private:
+};
+
+TEST_F(CommandFixture, basic_SSD_test_Write_1) {
+  executeAndExpectTRUE('W', 3, 0xAAAABBBB);
 }
 
-TEST(SSDTEST, basic_SSD_test_Write_2) {
-  char c = 'W';
-  int num = 3;
-  uint32_t hexValue = 0xAAAABBBB;
-  VirtualSSD ssd;
-  bool ret = ssd.executeCommand(c, num, hexValue);
-  num = 97;
-  hexValue = 0x1234ABCD;
-  ret = ssd.executeCommand(c, num, hexValue);
-  EXPECT_TRUE(ret);
+TEST_F(CommandFixture, basic_SSD_test_Write_2) {
+  executeAndExpectTRUE('W', 3, 0xAAAABBBB);
+  executeAndExpectTRUE('W', 97, 0x1234ABCD);
 }
 
-TEST(SSDTEST, basic_SSD_test_Read_3_TRUE) {
-  char c = 'R';
-  int num = 3;
-  uint32_t hexValue = 0xAAAABBBB;
-  VirtualSSD ssd;
-  bool ret = ssd.executeCommand(c, num, hexValue);
-  EXPECT_TRUE(ret);
+TEST_F(CommandFixture, basic_SSD_test_Read_3_TRUE)  
+{
+  executeAndExpectTRUE('R', 3, 0xAAAABBBB);
 }
 
-TEST(SSDTEST, basic_SSD_test_Read_3_FALSE) {
-  char c = 'R';
-  int num = 3;
-  uint32_t hexValue = 0x00000000;
-  VirtualSSD ssd;
-  bool ret = ssd.executeCommand(c, num, hexValue);
-  EXPECT_FALSE(ret);
+TEST_F(CommandFixture, basic_SSD_test_Read_3_FALSE) {
+  expectCommandFALSE('R', 3, 0x00000000);
 }
 
-TEST(SSDTEST, basic_SSD_test_Read_3_OutOfRange) {
-  char c = 'R';
-  int num = 103;
-  uint32_t hexValue = 0x00000000;
-  VirtualSSD ssd;
-  bool ret = ssd.executeCommand(c, num, hexValue);
-  EXPECT_FALSE(ret);
+TEST_F(CommandFixture, basic_SSD_test_Read_3_OutOfRange) {
+  expectCommandFALSE('R', 103, 0x00000000);
 }
 
-TEST(SSDTEST, basic_SSD_test_Write_Wrong_lba_index) {
-  char c = 'W';
-  int num = 102;
-  uint32_t hexValue = 0xAAAABBBB;
-  VirtualSSD ssd;
-  bool ret = ssd.executeCommand(c, num, hexValue);
-  EXPECT_FALSE(ret);
-}
+ TEST_F(CommandFixture, basic_SSD_test_Write_Wrong_lba_index) {
+   expectCommandFALSE('W', 102, 0xAAAABBBB);
+ }
