24cfd0a8bdba746d5655dc08971de72aa2100008
raina <raina.bak@gmail.com>
2025-04-03 11:37:48 +0900

    define.h 추가

---
+++ TeamB_SSD/define.h
@@ -0,0 +1,9 @@
+#pragma once
+
+static constexpr const char* ERROR = "ERROR";
+static constexpr const char* ERROR_SIZE = "ERROR_SIZE";
+static constexpr const char* ERROR_OUT_OF_RANGE = "ERROR_OUT_OF_RANGE";
+
+static constexpr const int MAX_RANGE_NUM = 100;
+static constexpr const int DEFAULT_VALUE = 0x00000000;
+#pragma once
