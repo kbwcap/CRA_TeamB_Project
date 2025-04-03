f31ac9de4cfdeafabdacfe3827586fda4442141e
bw.ko <kbwcap@gmail.com>
2025-04-04 03:45:47 +0900

    Add file IO into buffer directory

--- TeamB_SSD/CommandBuffer.h
+++ TeamB_SSD/CommandBuffer.h
@@ -12,26 +12,32 @@ class CommandBuffer {
 public:
   static constexpr int MAX_COMMANDS = 5;
 
-  CommandBuffer(VirtualSSD& ssd, const std::string& bufferFile = "command_buffer.txt");
+  CommandBuffer(VirtualSSD& ssd, 
+	  const std::string& bufferFile = "command_buffer.txt",
+	  const std::string& bufferFolder = "buffer");
 
   ~CommandBuffer();
 
   void addCommand(std::shared_ptr<ICommand> command);
-
   void clear();
-
   void reloadFromCommandFile();
+  void reloadFromCommandFile2();
+  void updateBufferFile(int index,
+	  const std::string& commandType,
+	  int lba, uint32_t dataOrSize);
+
 
 private:
-
+  void renameOrCreateFile(const std::string& oldFileName, const std::string& newFileName);
+  void createFile(const std::string& newFileName);
+  void initializeBufferFolder();
   void saveCommandToFile();
-
   void clearCommandFile();
-
   void excuteCommand();
 
   std::shared_ptr<ICommand> commandBuffer[MAX_COMMANDS] = {};
   int commandCount;
-  const std::string bufferFile;
+    const std::string bufferFile;
+	const std::string bufferFolder;
   VirtualSSD& ssd;
 };
