ac34ec7f6826880a4af291a05960234d11a6d624
raina <81407522+rainabak@users.noreply.github.com>
2025-04-03 12:01:47 +0900

    Update SSD_Test.cpp
    erase 테스트 추가

--- TeamB_SSD/SSD_Test.cpp
+++ TeamB_SSD/SSD_Test.cpp
@@ -7,7 +7,7 @@ using namespace testing;
 class CommandFixture : public Test {
  protected:
   VirtualSSD ssd;
-  std::shared_ptr<ICommand> makeCommand(char cmd, int lba, uint32_t value, int size) {
+  std::shared_ptr<ICommand> makeCommand(char cmd, int lba, uint32_t value) {
     std::shared_ptr<ICommand> ret;
     if (cmd == 'W') {
       ret = std::make_shared<WriteCommand>(ssd, lba, value);
@@ -16,20 +16,19 @@ class CommandFixture : public Test {
     } else if (cmd == 'F') {
       ret = std::make_shared<FlushCommand>(ssd);
     } else if (cmd == 'E') {
-      ret = std::make_shared<EraseCommand>(ssd, lba, size);
+      ret = std::make_shared<EraseCommand>(ssd, lba, value);
     }
     return ret;
   }
 
-  void executeAndExpectTRUE(char cmd, int lba = 0, uint32_t value = 0,
-                            int size = 0) {
-    std::shared_ptr<ICommand> Command = makeCommand(cmd, lba, value, size);
+  void executeAndExpectTRUE(char cmd, int lba = 0, uint32_t value = 0) {
+    std::shared_ptr<ICommand> Command = makeCommand(cmd, lba, value);
     bool ret = ssd.executeCommand(Command);
     EXPECT_TRUE(ret);
   }
 
-  void expectCommandFALSE(char cmd, int lba, uint32_t value, int size = 0) {
-    std::shared_ptr<ICommand> Command = makeCommand(cmd, lba, value, size);
+  void expectCommandFALSE(char cmd, int lba, uint32_t value) {
+    std::shared_ptr<ICommand> Command = makeCommand(cmd, lba, value);
     bool ret = ssd.executeCommand(Command);
     EXPECT_FALSE(ret);
   }
@@ -38,30 +37,34 @@ class CommandFixture : public Test {
 TEST_F(CommandFixture, basic_SSD_test_Write_1) {
   executeAndExpectTRUE('W', 3, 0xAAAABBBB);
 }
-//
-//TEST_F(CommandFixture, basic_SSD_test_Write_2) {
-//  executeAndExpectTRUE('W', 3, 0xAAAABBBB);
-//  executeAndExpectTRUE('W', 97, 0x1234ABCD);
-//}
-//
-//TEST_F(CommandFixture, basic_SSD_test_Read_3_TRUE) {
-//  executeAndExpectTRUE('R', 3, 0xAAAABBBB);
-//}
-//
-//TEST_F(CommandFixture, basic_SSD_test_Read_3_OutOfRange) {
-//  expectCommandFALSE('R', 103, 0x00000000);
-//}
-//
-//TEST_F(CommandFixture, basic_SSD_test_Write_Wrong_lba_index) {
-//  expectCommandFALSE('W', 102, 0xAAAABBBB);
-//}
-//
-//TEST_F(CommandFixture, basic_SSD_test_Flush) { executeAndExpectTRUE('F'); }
-//
-//TEST_F(CommandFixture, basic_SSD_test_Erase_0_10) {
-//  executeAndExpectTRUE('E', 0, DEFAULT_VALUE, 10);
-//}
-//
-//TEST_F(CommandFixture, basic_SSD_test_Erase_100_10) {
-//  expectCommandFALSE('E', 100, DEFAULT_VALUE, 10);
-//}
+
+TEST_F(CommandFixture, basic_SSD_test_Write_2) {
+  executeAndExpectTRUE('W', 3, 0xAAAABBBB);
+  executeAndExpectTRUE('W', 97, 0x1234ABCD);
+}
+
+TEST_F(CommandFixture, basic_SSD_test_Read_3_TRUE) {
+  executeAndExpectTRUE('R', 3, 0xAAAABBBB);
+}
+
+TEST_F(CommandFixture, basic_SSD_test_Read_3_OutOfRange) {
+  expectCommandFALSE('R', 103, 0x00000000);
+}
+
+TEST_F(CommandFixture, basic_SSD_test_Write_Wrong_lba_index) {
+  expectCommandFALSE('W', 102, 0xAAAABBBB);
+}
+
+TEST_F(CommandFixture, basic_SSD_test_Flush) { executeAndExpectTRUE('F'); }
+
+TEST_F(CommandFixture, basic_SSD_test_Erase_0_10) {
+  executeAndExpectTRUE('E', 0, 10);
+}
+
+TEST_F(CommandFixture, basic_SSD_test_Erase_95_10) {
+  executeAndExpectTRUE('E', 95, 10);
+}
+
+TEST_F(CommandFixture, basic_SSD_test_Erase_100_10) {
+  expectCommandFALSE('E', 100, 10);
+}
