ab7d722e1804673e4242d329a9dea2c44ee6739e
yuz010 <rladbwmd123@gmail.com>
2025-04-06 18:38:06 +0900

    [feature] script run by cmd

--- TeamB_Shell/main.cpp
+++ TeamB_Shell/main.cpp
@@ -1,7 +1,10 @@
 #include "ShellTest.h"
 #include "TestManager.h"
 #include "gmock/gmock.h"
-int main() {
+#include "define.h"
+#include <Windows.h>
+
+int main(int argc, char* argv[]) {
 #ifdef _DEBUG
   testing::InitGoogleTest();
   return RUN_ALL_TESTS();
@@ -9,6 +12,11 @@ int main() {
 
   TestManager::instance().loadTestSuitesDLL();
   ShellTest shellTest{};
+
+  if (argc == 2 && argv[1] == script_file_name) {
+    shellTest.executeCommand(script_file_name);
+  }
+
   while (true) {
     std::string input;
     std::cout << "> ";
